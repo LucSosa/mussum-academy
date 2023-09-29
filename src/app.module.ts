c2f80d0e0f134cddca70a7a3a533bd4652ae17f8
LucSosa <l.sosamachado@gmail.com>
2023-09-29 19:39:15 -0300

    :sparkles: Config Nestjs + GraphQl + TypeOrm

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
